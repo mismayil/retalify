dcb404f00312d7eaf5a3a3f5e09fff96f1374ad9
Riyad <riyad.hasanov@gmail.com>
2016-10-01 21:31:21 -0400

    Editing sign up page

--- public/routes/sign-up/sign-up.ctrl.js
+++ public/routes/sign-up/sign-up.ctrl.js
@@ -3,10 +3,13 @@ angular
     .controller('signUpController', signUpController);
 
 function signUpController($scope) {
-    $sope.user = {
+    $scope.user = {
     	firstName: 'Lorem Ipsum',
     	lastName: 'Memesh',
     	email: 'example@example.com',
+      about: 'gotun biri',
+      achievements: 'bir pox yemif',
+      country: 'USA',
     }
 
 }
